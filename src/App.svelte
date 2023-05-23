65b8f4ba48b1eb89548382e6767ed1c0a51d886b
Natouche68 <ngrossheny@gmail.com>
2023-05-23 19:13:50 +0200

    Add a simple prediction viewer

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import Topbar from './lib/Topbar.svelte';
+	import PredictionViewer from './lib/PredictionViewer.svelte';
+
+	import type { Prediction } from './lib/Predictions';
+
+	import { predictions } from './lib/Predictions';
+
+	const prediction: Prediction = predictions[Math.floor(Math.random() * predictions.length)];
 </script>
 
 <Topbar />
+<PredictionViewer {prediction} />
---
+++ src/lib/PredictionViewer.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type { Prediction } from './Predictions';
+
+	export let prediction: Prediction;
+</script>
+
+<div class="prediction-viewer">
+	<div class="icon">
+		{prediction.icon}
+	</div>
+	<div class="name">
+		{prediction.name}
+	</div>
+	<div class="message">
+		{prediction.message}
+	</div>
+</div>
+
+<style>
+	.prediction-viewer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 60vh;
+	}
+
+	.icon {
+		width: 128px;
+		height: 128px;
+		font-size: 100px;
+		text-align: center;
+	}
+
+	.name {
+		font-size: 48px;
+		font-weight: 600;
+		text-align: center;
+	}
+
+	.message {
+		font-size: 24px;
+		font-weight: 400;
+		text-align: center;
+		padding: 0 24px;
+	}
+</style>
