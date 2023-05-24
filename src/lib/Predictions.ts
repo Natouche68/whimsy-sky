c2572ad0b3385bfca87480a18011f200a39970e3
Natouche68 <ngrossheny@gmail.com>
2023-05-24 16:25:51 +0200

    Move the icons into a "icons" folder

--- src/lib/Predictions.ts
+++ src/lib/Predictions.ts
@@ -8,37 +8,37 @@ export type Prediction = {
 export const predictions: Prediction[] = [
 	{
 		name: 'Pizza Sun',
-		icon: 'pizza sun icon.png',
+		icon: 'icons/pizza sun icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: "Phew, there's no ananas on the sun. Only pepperoni, chorizo and ham.",
 	},
 	{
 		name: 'Cotton candy clouds',
-		icon: 'cotton candy clouds icon.png',
+		icon: 'icons/cotton candy clouds icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: 'Let your imagination take flight as the sky transforms into a sugary wonderland.',
 	},
 	{
 		name: 'Rainbow lightning',
-		icon: 'rainbow lightning icon.png',
+		icon: 'icons/rainbow lightning icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: 'Maybe this rainbow lightning gives you superpowers ? Or is there a treasure ?',
 	},
 	{
 		name: 'Hot chocolate floods',
-		icon: 'hot chocolate flood icon.png',
+		icon: 'icons/hot chocolate flood icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: "Considering the amount of chocolate, I think it's the Easter Bunny's secret reserve.",
 	},
 	{
 		name: 'Popcorn hail',
-		icon: 'popcorn hail icon.png',
+		icon: 'icons/popcorn hail icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: 'Free popcorn for your movie night ! You only need to choose the film.',
 	},
 	{
 		name: 'Cat rain',
-		icon: 'cat rain icon.png',
+		icon: 'icons/cat rain icon.png',
 		backgroundImage: 'https://tse3.mm.bing.net/th?id=OIP.u0enqBe9F9-DTt0jb63i6wAAAA&pid=Api',
 		message: "Don't forget to bring your favourite umbrella, but make sure it's cat-proof !",
 	},
